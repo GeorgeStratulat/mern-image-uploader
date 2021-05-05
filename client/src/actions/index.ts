8bb48e64755904dc0a34b6c5a8c592f99b605710
George Stratulat <george.stratulat@rei-d-services.com>
2021-05-05 21:28:11 +0300

    just the server

--- client/src/actions/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./types";
-export * from "./users";
-export * from "./auth";
-export * from "./alerts";
--- client/src/actions/users.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-  active?: boolean;
-  id?: string;
-  role?: string;
-  email?: string;
-  images?: [string];
-  password?: string;
-  passwordChangedAt?: Date;
-}
